f0d2627df5b0a22a1cef3556d4aff0510632530d
Orozbek <tagaev.orozbek@gmail.com>
2023-11-24 14:30:08 +0600

    create project

---
+++ src/components/main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1> Hello World</h1>
+            <p>test js</p>
+        </div>
+    );
+};
+
+export default Main ;
